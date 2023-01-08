69463c4fcca2c85d150023fb1c56518daae495d1
Diogo Paza <diogopazacvel@gmail.com>
2023-01-08 20:20:27 -0300

    finally AvaliacaoService

--- erros/src/main/java/com/erros/controller/AvaliacaoController.java
+++ erros/src/main/java/com/erros/controller/AvaliacaoController.java
@@ -1,11 +1,10 @@
 package com.erros.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erros.DTO.AvaliacaoDTO;
@@ -20,19 +19,17 @@ public class AvaliacaoController {
 
     AvaliacaoService avaliacaoServico;
 
-    @PostMapping
-    public String cadastrar(@RequestBody AvaliacaoDTO avaliacao){
-        try{
-            System.out.println(avaliacao.getNome());
-            var result = avaliacaoServico.create(avaliacao);
-           // return ResponseEntity.status(HttpStatus.OK).body(null);
-           return result;
-        }catch(Exception ex){
+    @PostMapping(produces =  MediaType.APPLICATION_PDF_VALUE)
+    public  byte[] cadastrar(@RequestBody AvaliacaoDTO body) {
+        try {           
+           // System.out.println(avaliacao.getNome());
+            var result = avaliacaoServico.create(body);            
+            return result;
+        } catch (Exception ex) {
             System.out.println("ERRO" + ex);
-            //ResponseEntity.badRequest().build();
-            return "erro";
-           
+            return null;
         }
+
     }
-    
+
 }
--- erros/src/main/java/com/erros/service/AvaliacaoService.java
+++ erros/src/main/java/com/erros/service/AvaliacaoService.java
@@ -1,39 +1,61 @@
 package com.erros.service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
-import org.springframework.stereotype.Service;
-import com.erros.DTO.AvaliacaoDTO;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.parser.clipper.Paths;
+
+import org.apache.pdfbox.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.erros.DTO.AvaliacaoDTO;
+
+import lombok.AllArgsConstructor;
 
 @Service
 public class AvaliacaoService {
-    public String create(AvaliacaoDTO avaliacao) {
-        System.out.println("AVALIACAO: ");
-        savePDF(avaliacao);
-        return "create";
 
+    public byte[] create(AvaliacaoDTO dto)  {
+
+        try {
+            var myPDF = savePDF(dto);
+            byte[] array = Files.readAllBytes(java.nio.file.Paths.get(myPDF));
+            return array;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
     }
 
+    @Async
     public String savePDF(AvaliacaoDTO avaliacao) {
         var document = new Document();
         try {
             var data = LocalDateTime.now();
             DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd-MM-uuuu-hh-mm");
-            String dataFormatada = formatterData.format(data);           
-            var arquivoPDF = String.format(".\\%s-%s.pdf", avaliacao.getNome(),dataFormatada).replaceAll(" ", "");            
+            String dataFormatada = formatterData.format(data);
+            var arquivoPDF = String.format("./%s-%s.pdf", avaliacao.getNome(), dataFormatada).replaceAll(" ", "");
             PdfWriter.getInstance(document, new FileOutputStream(arquivoPDF));
             document.open();
             document.add(new Paragraph(avaliacao.getNome()));
             document.add(new Paragraph(avaliacao.getNota()));
             document.close();
-            return "create";
+            return arquivoPDF;
         } catch (DocumentException ex) {
             System.out.println("Erro ao gerar PDF" + ex);
             return "erro";
